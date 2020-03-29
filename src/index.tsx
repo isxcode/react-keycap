c8ca5f949a95b5fdc392aa41037141a0375c9182
ispong <ispong@outlook.com>
2020-03-29 16:38:15 +0800

    [update] 尝试封装layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import RouterManager from "./RouterManager";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 
-ReactDOM.render(<RouterManager />, document.getElementById("root"));
+import home from './home/Home';
+import spring_oxygen_index from './spring-oxygen/Index';
+import react_keycap_index from './react-keycap/Index';
+
+// 这里如入口,傻瓜式开发,拿到首页的div标签root,进行指定渲染,最好配置路由
+ReactDOM.render(
+	<div>
+		<BrowserRouter>
+			<Redirect to={'/spring-oxygen'} />
+			{/*<Route path='/' component={home} />*/}
+			<Route path='/spring-oxygen' component={spring_oxygen_index} />
+			<Route path='/react-keycap' component={react_keycap_index} />
+		</BrowserRouter>
+	</div>,
+	document.getElementById('root'),
+);
