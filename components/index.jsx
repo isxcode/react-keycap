728fedf553764304b197adf167e2e56dd8b6dc23
ispong <ispong@outlook.com>
2020-08-04 11:23:08 +0800

    :fire: remove rollup / it has bug

--- components/index.jsx
+++
@@ -1,8 +0,0 @@
-// 测试对象使用
-export { KDemo } from './demo/kdemo'
-
-// // 头像组件
-// export { default as KAvatar } from './avatar/kavatar'
-//
-// // 按钮组件
-// export { default as KButton } from './button/kbutton'
---
+++ components/index.tsx
@@ -0,0 +1,8 @@
+// 测试对象使用
+export { default as KDemo } from './demo/kdemo'
+
+// 头像组件
+export { default as KAvatar } from './avatar/kavatar'
+
+// 按钮组件
+export { default as KButton } from './button/kbutton'
