bf900a64c8c8a77ee82fc40095e30b143ac399dc
ispong <ispong@outlook.com>
2021-05-16 15:55:57 +0800

    :bug: fix docs deploy script

--- docs/deploy/deploy.sh
+++ docs/deploy/deploy.sh
@@ -1,12 +1,9 @@
-#  deploy keycap docs
-# =======
+# deploy keycap docs
 #
 # sudo mkdir -p /data/keycap
 # cd /data/keycap
-# git clone -b latest https://github.com/isxcode/react-keycap
+# sudo git clone -b latest https://github.com/isxcode/react-keycap
+# cd /data/keycap/react-keycap/ && bash docs/deploy/deploy.sh
 
-# 打包镜像
-docker build -t isxcode/keycap-docs:0.0.1 -f /data/keycap/react-keycap/docs/deploy/Dockerfile
-
-# 运行镜像
-docker run --restart=always --name keycap-docs -p 30013:80 -d isxcode/keycap-docs:0.0.1
+# build and run docs image
+docker build -t isxcode/keycap-docs:0.0.1 -f /data/keycap/react-keycap/docs/deploy/Dockerfile . && docker run --restart=always --name keycap-docs -p 30013:80 -d isxcode/keycap-docs:0.0.1
